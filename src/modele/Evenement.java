dc7ba89de77cc94bf3777c6ec189320a7a9c85e7
Samuel <samuellaroche@live.ca>
2018-04-10 00:25:48 -0400

    update evenement

--- src/modele/Evenement.java
+++ src/modele/Evenement.java
@@ -1,25 +1,32 @@
 package modele;
 
+import java.time.LocalDateTime;
+
 public class Evenement {
-	
+	//Les constantes représente un facteur de congestion.
 	public static final float NULL = 1;
 	public static final float LOW = (float) 1.25;
 	public static final float MEDIUM = (float) 1.5;
 	public static final float HIGH = 2;
 	public static final float VERYHIGH = 3;
-	public static final float FULL = 0;
+	public static final double FULL = Double.MAX_VALUE; //Ou infini. Le liens sera toujours écarter par Djikstra
 	
-	private float congestion;
+	private double congestion;
+	private LocalDateTime time;
 	
 	public Evenement(){
-		this.setCongestion(NULL);
+		this.setCongestion(NULL); //Par défault la congestion est nulle
+	}
+	public Evenement(double congestion){
+		this.setCongestion(congestion);
+		this.time = LocalDateTime.now();
 	}
 	
 	public double getCongestion(){
 		return this.congestion;
 	};
 	
-	public void setCongestion(float level){
+	public void setCongestion(double level){
 		congestion = level;
 	};
 
--- src/modele/Lien.java
+++ src/modele/Lien.java
@@ -8,7 +8,7 @@ public class Lien {
 	private Noeud a,b;
 	private double poid; //distance......deffinir la distance comme un int
 	private Integer index;
-	private Evenement evenement;
+	public Evenement evenement;
 	
 	public Lien(Noeud a, Noeud b){
 		/**Créer un lien et calcule automatiquement la distance (ou poid) selon l'hypotenuse des coordonnées déclarer dans 
@@ -19,7 +19,6 @@ public class Lien {
 		this.b = b;	
 		this.poid = Math.hypot(b.getX()-a.getX(), b.getY()-a.getY()); // calculer le poid en precision int donc b.getCoordonnees().x
 		this.index = (a.getNom().hashCode() + b.getNom().hashCode());
-		evenement = new Evenement();
 		
 	}
 	
@@ -29,22 +28,32 @@ public class Lien {
 	public void setPoid(Double poid){
 		this.poid = poid;
 	}
+	
 	public Double getPoid(){
 		/** La méthode ajuste le poid à la hausse si l'heure courante s'approche des heures de pointes. 
 		 * Les heures de haute densité sont 8h et 17h.
 		 * La densité suit un fonction cosinus entre 4h et 21h, la densité est constante à l'exterieur de ces heures.
 		 * Plus on s'approche de 8h et 17h, plus le poid (temps) retourné est élevé.
 		 * Lors des pointes, le facteur maximum est 3x le poid normal.
+		 * 
+		 * Si un évènement (traffic ou accident) affecte la circulation, le poid est haussé.
 		 * @author Samuel
 		 */
 		double poid;
 		double time = (double) LocalTime.now().getHour() + (double) LocalTime.now().getMinute() / 60;;
 		if (3.94 < time && time < 20.905){
+			//poid = poid départ (distance) * facteur heures de pointes * facteur evenement
 			poid = this.poid * (Math.cos(time / 1.35 + 0.7 / Math.PI ) + 2);
+		} else {
+			poid = this.poid;
+		}
+		if (evenement == null) {
 			return poid;
-		} else
-		return this.poid;
+		} else {
+			return poid * evenement.getCongestion();
+		}
 	}
+	
 	public Double comparePoid(Lien autre){
 		return this.poid - autre.getPoid();
 	}
@@ -64,7 +73,7 @@ public class Lien {
 	public Noeud getNoeudDeux(){
 		return this.b;
 	}
-	//override
+
 	public int hashCode(){
 		int hash = a.getNom().hashCode() + b.getNom().hashCode();
 		return hash;
